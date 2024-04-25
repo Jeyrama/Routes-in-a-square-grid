447e387021ac3f7022b040145358b2f60d7935a7
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-25 15:34:31 -0700

    Update Routes-in-a-square-grid.js

--- Routes-in-a-square-grid.js
+++ Routes-in-a-square-grid.js
@@ -22,3 +22,21 @@ Note:
 const partialFactorial = (t, b) => Array(...Array(t - b)).map((_, i) => i + b + 1).reduce((p, c) => p * c, 1);
 const factorial = n => partialFactorial(n, 1);
 const routes = n => n > 0 ? Math.round(partialFactorial(2 * n, n) / factorial(n)) : 0;
+
+// or
+
+function routes(n){
+  if(n<1)return 0;
+  let arr1=[];
+  for(let i=0;i<=n;i++)arr1.push(1);
+  let arr2;
+  for(let i=0;i<n;i++){
+  	let arr2=new Array(n+1);
+  	arr2[0]=1;
+    for(let j=1;j<=n;j++){
+       arr2[j]=arr2[j-1]+arr1[j];
+    }
+    arr1=arr2;
+  }
+  return arr1[n];
+}
